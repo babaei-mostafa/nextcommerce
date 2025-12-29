aa97753f7a1a4d7800313812bdf0e3d8bb3b013c
lucas-hood <mostafa.bsh@gmail.com>
2025-12-29 23:31:06 +0800

    created a mobile menu and product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 export const metadata = {
   title: "Home",
 };
 
 const HomePage = () => {
-  return <>Nextcommerce</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={3} />
+    </>
+  );
 };
 
 export default HomePage;
