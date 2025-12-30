557a262ba24f9c5d4eda00d38da5fa87b738eb3e
lucas-hood <mostafa.bsh@gmail.com>
2025-12-30 21:59:00 +0800

    created seed.js file and seeded sample data to database

---
+++ db/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+import sampleData from "./sample-data";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+async function main() {
+  console.log(process.env.DATABASE_URL);
+  const prisma = new PrismaClient({
+    adapter,
+  });
+  try {
+    await prisma.product.deleteMany();
+    await prisma.product.createMany({ data: sampleData.products });
+    console.log("Database seeded successfully");
+  } catch (err) {
+    console.error("Seeding failed:", err);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
